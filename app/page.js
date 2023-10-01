c5aeb7613ad39b5c3e03c4e19e9df80a627fcc51
Maruf Hosen <108074496+marufhosen15@users.noreply.github.com>
2023-10-01 13:15:33 +0600

    Updated

--- app/page.js
+++ app/page.js
@@ -7,44 +7,44 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-    const [allPlaugins, setAllPlugins] = useState([]);
-    const { registerPlugin, plugins } = usePluginContext();
+  const [allPlaugins, setAllPlugins] = useState([]);
+  const { registerPlugin, plugins } = usePluginContext();
 
-    // REGISTER ALL COMPONENTS
-    useEffect(() => {
-        allPlaugins?.forEach((component) => {
-            registerPlugin(component);
-        });
-    }, [allPlaugins]);
+  // REGISTER ALL COMPONENTS
+  useEffect(() => {
+    allPlaugins?.forEach((component) => {
+      registerPlugin(component);
+    });
+  }, [allPlaugins]);
 
-    // GET ALL FILE NAMES FROM A PLUGIN FOLDER
-    useEffect(() => {
-        const getData = async () => {
-            const res = await axios("/api/getComponentFile", {
-                headers: { "Content-Type": "application/json" },
-            });
-            setAllPlugins(res.data.fileContent);
-        };
-        getData();
-    }, []);
+  // GET ALL FILE NAMES FROM A PLUGIN FOLDER
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios("/api/getComponentFile", {
+        headers: { "Content-Type": "application/json" },
+      });
+      setAllPlugins(res.data.fileContent);
+    };
+    getData();
+  }, []);
 
-    console.log("plugins", plugins);
+  console.log("plugins", plugins);
 
-    return (
-        <Layout className='flex min-h-screen flex-col items-center justify-between p-24'>
-            <h1 className='text-4xl font-bold text-center'>Home</h1>
-            <PluginRenderer plugins={plugins} />
-        </Layout>
-    );
+  return (
+    <Layout className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold text-center">Home</h1>
+      <PluginRenderer plugins={plugins} />
+    </Layout>
+  );
 }
 
 // RENDER ALL PLUGIN COMPONENTS PLUGIN
 const PluginRenderer = ({ plugins }) => {
-    return (
-        <>
-            {plugins?.map((Plugin, index) => (
-                <Plugin key={index} />
-            ))}
-        </>
-    );
+  return (
+    <>
+      {plugins?.map((Plugin, index) => (
+        <Plugin key={index} />
+      ))}
+    </>
+  );
 };
